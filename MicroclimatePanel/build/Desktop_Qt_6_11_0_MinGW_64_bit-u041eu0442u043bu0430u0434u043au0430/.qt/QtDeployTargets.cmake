e21ba399c53632ba34174c9f99bb30591ba2d3b5
Vasil <vasil.tarazov@mail.ru>
2026-05-18 16:31:21 +0500

    ready

--- MicroclimatePanel/build/Desktop_Qt_6_11_0_MinGW_64_bit-u041eu0442u043bu0430u0434u043au0430/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_MicroclimatePanel_FILE C:/creativeProject/MicroclimatePanel/build/Desktop_Qt_6_11_0_MinGW_64_bit-u041eu0442u043bu0430u0434u043au0430/MicroclimatePanel.exe)
-set(__QT_DEPLOY_TARGET_MicroclimatePanel_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_MicroclimatePanel_RUNTIME_DLLS C:/Qt/6.11.0/mingw_64/bin/Qt6Network.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6PrintSupport.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Core.dll)
